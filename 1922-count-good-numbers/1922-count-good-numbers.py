8782b1c3124bbb9e7c528206c04a9d42324b643e
Heiliger <137763215+Heiliger4@users.noreply.github.com>
2025-03-05 18:02:19 +0300

    Time: 0 ms (100%), Space: 17.8 MB (43.81%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.py
+++ 1922-count-good-numbers/1922-count-good-numbers.py
@@ -4,6 +4,4 @@ class Solution:
         evenPos = (n + 1) // 2
         oddPos = n // 2
         combination = (pow(5, evenPos, MOD) * pow(4, oddPos, MOD)) % MOD
-        # combination = (5 ** evenPos) * (4** oddPos)
-        # combination = combination % (10**9 + 7)
         return combination
