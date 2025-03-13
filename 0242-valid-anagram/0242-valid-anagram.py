6c0aecfbaa9f13f2efef416096a4f53fedabd236
Heiliger <137763215+Heiliger4@users.noreply.github.com>
2025-03-13 23:03:13 +0300

    Time: 15 ms (43.24%), Space: 18.7 MB (11.82%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,14 +1,20 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(t) == sorted(s)
+'''
+        #is diff len: early out
         if len(s) != len(t):
             return False
-
+        
+        #make a list of the incomming string then iterate over the main string while removing each ele from the list if they appear on the main string
         tmp_list = list(t)
         for i in s:
             if i in tmp_list:
                 tmp_list.remove(i)
             else:
                 return False
-
+#if all gets removed then ANAGRAM
         return len(tmp_list) == 0
+'''
+
             
