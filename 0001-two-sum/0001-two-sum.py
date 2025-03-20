e8e1f4105a27b813daa9239e7c61c769c6c71bd5
Heiliger <137763215+Heiliger4@users.noreply.github.com>
2025-03-20 19:26:26 +0300

    Time: 1708 ms (27.31%), Space: 18.3 MB (87.26%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,7 +1,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         # check each number to the rest of the array if it can make the target #no
-        # if so find the index of both elements
+        # if so return index of both elements
         n = len(nums)
         for i in range(n):
             for j in range(i+1, n):
