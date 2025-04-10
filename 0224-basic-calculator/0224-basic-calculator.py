2898130404bd1ea6253a2ee921250e785db4022b
Heiliger <137763215+Heiliger4@users.noreply.github.com>
2025-04-10 13:05:31 +0300

    Time: 19 ms (94.82%), Space: 18.4 MB (63.13%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -0,0 +1,31 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        stack = []
+        current_number = 0
+        sign = 1  # 1 means positive, -1 means negative
+        result = 0
+
+        for char in s:
+            if char.isdigit():
+                current_number = current_number * 10 + int(char)
+            elif char == '+':
+                result += sign * current_number
+                current_number = 0
+                sign = 1
+            elif char == '-':
+                result += sign * current_number
+                current_number = 0
+                sign = -1
+            elif char == '(':
+                stack.append(result)
+                stack.append(sign)
+                result = 0
+                sign = 1
+            elif char == ')':
+                result += sign * current_number
+                current_number = 0
+                result *= stack.pop()  # sign before the parenthesis
+                result += stack.pop()  # result before the parenthesis
+
+        result += sign * current_number
+        return result
