9efb8036c6a5ccf350014cb6637a9c6245bdf79a
Heiliger <137763215+Heiliger4@users.noreply.github.com>
2025-04-15 03:09:51 +0300

    Time: 0 ms (100%), Space: 17.7 MB (83.8%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def isMirror(t1: Optional[TreeNode], t2: Optional[TreeNode]) -> bool:
+            if not t1 and not t2:
+                return True
+            if not t1 or not t2:
+                return False
+            return (t1.val == t2.val and
+                    isMirror(t1.left, t2.right) and
+                    isMirror(t1.right, t2.left))
+        
+        return isMirror(root, root)
